0b8803e23b23e336e5a77677af47119827524e5e
Raphael Krut-Landau <raphael.kl@gmail.com>
2014-02-11 00:13:13 +0100

    documentation

--- pdfthumbnail.sh
+++ pdfthumbnail.sh
@@ -6,14 +6,16 @@ png="$file.png"
 convert -density 300 $file[0] -quality 100 $png
 # add later: -threshold 70% 
 convert -crop 500x500 +repage $png $png-box%02d.png
-# test the boxes
+
+# Which box has more black pixels than any other?
 find . | grep $png-box.*png | while read boxfile; do
     echo -n "$boxfile"$'\t' >> temp_box_data
     convert $boxfile -format %c histogram:info: | grep black | cut -d ":" -f 1 | tr -d ' ' >> $png-temp_box_data
 done
 best_thumbnail=`cat $png-temp_box_data | sort -bgrk2 --field-separator=$'\t' | head -n 1 | cut -d $'\t' -f 1`
 convert -resize 250x250 $best_thumbnail $best_thumbnail
-# test the boxes
+
+# clean up
 rm $png-temp_box_data
-cat $best_thumbnail
+cat $best_thumbnail # outputs the file itself
 find . -name "$png-box*png" -print0 | xargs -0 rm
